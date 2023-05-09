00c32f325288a34388462a244bad0c1e47f1a253
zhangguohao <472365411@qq.com>
2023-05-09 18:12:06 +0800

    dev环境地址更改

--- config/constant.js
+++ config/constant.js
@@ -2,7 +2,7 @@
  * @constant API_ENV_MAP - API请求的环境变量映射
  */
 export const API_ENV_MAP = {
-  dev: 'http://192.168.118.85:9528',
+  dev: 'http://localhost:9528',
   test: 'http://192.168.118.86:9528',
   pre: 'http://192.168.118.87:9528',
 };
